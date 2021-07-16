5ab4c6c43a4dbec8f4bd3b4e10db9e7795ed012e
ArielAxionL <i@axionl.me>
2021-07-16 15:27:06 +0800

    update some options

--- completions/home-manager.fish
+++ completions/home-manager.fish
@@ -13,7 +13,7 @@ function __home_manager_generations
 end
 
 complete -c home-manager -n "__fish_use_subcommand" -f -a "help" -d "Print home-manager help"
-complete -c home-manager -n "__fish_use_subcommand" -f -a "edit" -d 'Open the home configuration in $EDITOR'
+complete -c home-manager -n "__fish_use_subcommand" -f -a "edit" -d "Open the home configuration in $EDITOR"
 complete -c home-manager -n "__fish_use_subcommand" -f -a "build" -d "Build configuration into result directory"
 complete -c home-manager -n "__fish_use_subcommand" -f -a "instantiate" -d "Instantiate the configuration and print the resulting derivation"
 complete -c home-manager -n "__fish_use_subcommand" -f -a "switch" -d "Build and activate configuration"
@@ -28,15 +28,15 @@ complete -c home-manager -n "__fish_seen_subcommand_from remove-generations" -f 
 complete -c home-manager -n "__fish_use_subcommand" -x -a "expire-generations" -d "Remove generations older than TIMESTAMP"
 
 ### Options
-complete -c home-manager -n "__fish_no_arguments" -x -s f -d "The home configuration file"
-complete -c home-manager -n "__fish_no_arguments" -x -s A -d "Optional attribute that selects a configuration expression in the configuration file"
-complete -c home-manager -n "__fish_no_arguments" -x -s I -d "Add a path to the Nix expression search path"
-complete -c home-manager -n "__fish_no_arguments" -x -s b -d "Move existing files to new path rather than fail"
-complete -c home-manager -n "__fish_no_arguments" -f -s v -d "Verbose output"
-complete -c home-manager -n "__fish_no_arguments" -f -s n -d "Do a dry run, only prints what actions would be taken"
-complete -c home-manager -n "__fish_no_arguments" -f -s h -l "help" -d "Print this help"
+complete -c home-manager -F -s f -l "file" -d "The home configuration file"
+complete -c home-manager -x -s A -d "Optional attribute that selects a configuration expression in the configuration file"
+complete -c home-manager -F -s I -d "Add a path to the Nix expression search path"
+complete -c home-manager -F -s b -d "Move existing files to new path rather than fail"
+complete -c home-manager -f -s v -l "verbose" -d "Verbose output"
+complete -c home-manager -f -s n -l "dry-run" -d "Do a dry run, only prints what actions would be taken"
+complete -c home-manager -f -s h -l "help" -d "Print this help"
 
-complete -c home-manager -x -l cores -d "NUM"
+complete -c home-manager -x -l "cores" -d "NUM"
 complete -c home-manager -f -l "keep-failed"
 complete -c home-manager -f -l "keep-going"
 complete -c home-manager -x -s j -l "max-jobs" -d "NUM"
